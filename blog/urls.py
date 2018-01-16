f0bf310edd802ee8779df81cb76cd15b5824db0c
Alan's Macbook <Alan@Alans-MacBook-Pro.local>
2018-01-16 14:14:18 +0800

    create base

--- blog/urls.py
+++ blog/urls.py
@@ -15,7 +15,18 @@ Including another URLconf
 """
 from django.contrib import admin
 from django.urls import path
+from django.conf import settings
+from django.conf.urls import  include, url
+from django.conf.urls.static import static
 
 urlpatterns = [
-    path('admin/', admin.site.urls),
+    (r'^$','blogpost.views.index'),
+    url(r'^blog/(?P<slug>[^\.]+).html','blogpost.views.view_post',name='view_blog_post'),
+    url(r'^admin/',include(admin.site.urls))
 ]
+
+# 在上面的代码里，我们创建了两个route：
+
+# 指向首页，其view是index
+# 指向博客详情页，其view是view_post
+# 指向博客详情页的URL正则r'^blog/(?P<slug>[^\.]+).html，会将形如blog/hello-world.html中的hello-world提取出来作为参数传给view_post方法。
--- blogpost/views.py
+++ blogpost/views.py
@@ -1,3 +1,23 @@
 from django.shortcuts import render
+from django.shortcuts import render,render_to_response,get_object_or_404
+from blogpost.models import Blogpost
 
-# Create your views here.
+# 对于我们的首页来说，我们可以简单的只显示五篇博客，所以我们所需要做的就是从我们的Blogpost对象中，取出前五个结果即可。
+
+def index(request):
+    return render_to_response('index.html', {
+        'posts': Blogpost.objects.all()[:5]
+    })
+
+# Django的render_to_response方法可以根据一个给定的上下文字典渲染一个给定的目标，并返回渲染后的HttpResponse。即将相应的值，
+# 如这里的Blogpost.objects.all()[:5]，填入相应的index.html中，再返回最后的结果。
+
+
+# 依据拿到的slug，我们就可以创建对应的详情页的view，代码如下所示：
+
+def view_post(request, slug):
+    return render_to_response('blogpost_detail.html', {
+        'post': get_object_or_404(Blogpost, slug=slug)
+    })
+
+# 这里的get_object_or_404将会根据slug来获取相应的博客，如果取不出相应的博客就会返回404。因此，我们的详情页和上面的列表页也是类似的。
